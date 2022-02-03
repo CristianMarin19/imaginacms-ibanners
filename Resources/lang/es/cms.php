97d7e89be453f2ceea80dd6d1cafa3aff7b976a8
Michael <msolano@imaginacolombia.com>
2022-02-03 12:36:50 -0500

    Add: CMS Translations

---
+++ Resources/lang/es/cms.php
@@ -0,0 +1,12 @@
+<?php
+return [
+  "newBanner" => 'Nuevo Banner',
+  "updateSlider" => 'Actualizar Banner',
+  "createSlider" => 'Crear Banner',
+  "newBannerItem" => 'Nuevo Elemento',
+  "updateSlide" => 'Actualizar Elemento',
+  "label" => [
+    "codeAds"  => 'Codigo ADS',
+    "showAsPopup" => 'Mostrar como ventana emergente'
+  ]
+];
